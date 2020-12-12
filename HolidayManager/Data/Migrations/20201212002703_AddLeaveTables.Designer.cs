65ea116f9d25d829661f14e290c94b436cabfb34
Iain <iain.stewart08@gmail.com>
2020-12-12 00:37:14 +0000

    Add db contexts and perform migrations

--- HolidayManager/Data/Migrations/20201212002703_AddLeaveTables.Designer.cs
+++ HolidayManager/Data/Migrations/20201212002703_AddLeaveTables.Designer.cs
@@ -4,14 +4,16 @@ using HolidayManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HolidayManager.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201212002703_AddLeaveTables")]
+    partial class AddLeaveTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
