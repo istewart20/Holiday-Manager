b2a87e8ceeb72d9d57d9d52dfc8b21a2074e1e10
Iain <iain.stewart08@gmail.com>
2020-12-17 20:51:47 +0000

    Setup ViewModels and install Automapper

---
+++ HolidayManager/Models/LeaveTypeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HolidayManager.Models
+{
+    public class DetailsLeaveTypeViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+    }
+
+    public class CreateLeaveTypeViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
